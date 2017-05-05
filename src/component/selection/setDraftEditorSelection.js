4885c463f8d70db45e171b791b265e4f533468d4
Holsopple, Dirk <dholsopple@cvent.com>
2017-05-05 12:36:25 -0400

    Add check for selection type of None

--- src/component/selection/setDraftEditorSelection.js
+++ src/component/selection/setDraftEditorSelection.js
@@ -218,7 +218,8 @@ function addFocusToSelection(
   offset: number,
   selectionState: SelectionState,
 ): void {
-  if (selection.extend && containsNode(getActiveElement(), node)) {
+  if (selection.extend && containsNode(getActiveElement(), node) &&
+    selection.type !== 'None') {
     // If `extend` is called while another element has focus, an error is
     // thrown. We therefore disable `extend` if the active element is somewhere
     // other than the node we are selecting. This should only occur in Firefox,
